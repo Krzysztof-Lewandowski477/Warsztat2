a27bec64a3ee9beef9afc5961e7ce2b3fc7bbe74
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-10-29 18:09:41 +0200

    changes

--- pl/coderslab/controller/UserController.java
+++ pl/coderslab/controller/UserController.java
@@ -1,23 +1,25 @@
 package pl.coderslab.controller;
 
+import pl.coderslab.dao.UserDao;
 import pl.coderslab.entity.User;
 import pl.coderslab.service.UserService;
 import pl.coderslab.utils.DBUtil;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Scanner;
 
-public class UserController {
+public class UserController  {
+
 
     private final UserService userService;
+
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     public void showMenu(){
+        Scanner scan = new Scanner(System.in);
         System.out.println("Wybierz opcje");
         System.out.println("1 : Display all");
         System.out.println("2 : Display one");
@@ -27,48 +29,32 @@ public class UserController {
         System.out.println("6 : powrót do menu");
 
 
-        //  private User[] addToArray(User u, User[] users) {
-        //        User[] tmpUsers = Arrays.copyOf(users, users.length + 1);
-        //        tmpUsers[users.length] = u;
-        //        return tmpUsers;
-        //    }
-    }
-
-    public void displayAllUsers(){
-//         public User read(int userId) {
-//                try (Connection conn = DBUtil.getConnection()) {
-//                    PreparedStatement statement = conn.prepareStatement(READ_USER_QUERY);
-//                    statement.setInt(1, userId);
-//                    ResultSet resultSet = statement.executeQuery();
-//                    if (resultSet.next()) {
-//                        User user = new User();
-//                        user.setId(resultSet.getInt("id"));
-//                        user.setUsername(resultSet.getString("username"));
-//                        user.setEmail(resultSet.getString("email"));
-//                        user.setPassword(resultSet.getString("password"));
-//
-//                        return user;
-//                    }
-//                } catch (SQLException e) {
-//                    e.printStackTrace();
-//                }
-//                return null;
-//            }
-    }
-
-    public void displayOneUser(){
 
     }
 
-    public void createUser(){
+
+    public void displayAllUsers (User[] findAll) {
+        userService.findAll();
 
     }
 
-    public void updateUser(){
+    public void displayOneUser(Integer id){
+        userService.readById(id);
 
     }
 
-    public void deleteUser(){
+    public void createUser(User user){
 
+         userService.create(user);
+         return;
+    }
+
+    public void updateUser(User user){
+        userService.update(user);
+    }
+
+    public void deleteUser(Integer id){
+        userService.delete(id);
     }
 }
+
