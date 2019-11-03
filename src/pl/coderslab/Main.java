475232a03b1e78448d346666b07913f25f680564
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-11-03 11:46:06 +0200

    changes

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -1,6 +1,13 @@
 package src.pl.coderslab;
 
+import src.pl.coderslab.controller.UserController;
+import src.pl.coderslab.service.UserService;
+
+import java.util.Scanner;
+
 public class Main {
+
+
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -16,7 +23,14 @@ public class Main {
         System.out.println("1 Użytkownikami ");
         System.out.println("2 Grupami");
         //TODO : wczytać i użyć kontrollera odpowiedniego
+        Scanner scann1 = new Scanner(System.in);
+        while(!scann1.hasNextInt()){
+            System.out.println("Podaj liczbę wyboru");
 
+
+        }
+
+        UserController user = new UserController().showMenu();
 
     }
 }
